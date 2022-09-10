0b07443758866b4de7badfb43d0a0f91bc1a255a
galihsamudrosulaksono <galihsamoo07@gmail.com>
2022-09-10 08:31:15 +0700

    Adding guarded on all model

--- app/Models/LetterHead.php
+++ app/Models/LetterHead.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class LetterHead extends Model
 {
     use HasFactory;
+    protected $guarded = ['id', 'timestamps'];
 }
--- app/Models/ModerationComment.php
+++ app/Models/ModerationComment.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class ModerationComment extends Model
 {
     use HasFactory;
+    protected $guarded = ['id', 'timestamps'];
 }
