7f15b69e3ff01fa5e5607f5dffbddcec866d73fd
galihsamudrosulaksono <galihsamoo07@gmail.com>
2022-09-10 09:56:45 +0700

    Adding role on user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('email')->index();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['user', 'admin'])->default('user');
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletesTz($column = 'deleted_at', $precision = 0);
