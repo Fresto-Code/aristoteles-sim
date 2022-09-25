06e986d24e69179d8a2ac15b6b5720f799bbe639
warwijk <ivangiovn@gmail.com>
2022-09-25 20:40:01 +0700

    Revise head

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Argon Dashboard') }}</title>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
         <!-- Favicon -->
         <link href="{{ asset('argon') }}/img/brand/favicon.png" rel="icon" type="image/png">
         <!-- Fonts -->
